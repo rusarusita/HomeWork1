baa829ceb434680eeede1a4a2ccb85ffeb0df1b0
Rusudan Rusitashvili <rusudan.rusitashvili@tera.ge>
2023-02-16 14:46:28 +0400

    initial

---
+++ HomeWork/Program.cs
@@ -0,0 +1,15 @@
+﻿class rusashomework
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("rusa");
+
+        Console.WriteLine("gamarjoba");
+
+        Console.WriteLine("rogor xar");
+
+        Console.WriteLine("kargad");
+
+    }
+
+}
