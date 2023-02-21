0c398bff08e8c572768605ef64ab1821293a04fe
rusudan.rusitashvili <rusudan.rusitashvili@RUSITASHVILILEP>
2023-02-21 17:47:35 +0400

    comit

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,19 +1,45 @@
-﻿class rusashomework
+﻿class rusashomework2
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("rusa");
 
-        Console.WriteLine("gamarjoba");
+        Console.Write("Gtxovt sheiyvanot X ricxvi: ");
+        int x = Int32.Parse(Console.ReadLine());
+        Console.Write("Gtxovt sheiyvanot y ricxvi: ");
+        int y = Int32.Parse(Console.ReadLine());
+        Console.Write("Gtxovt sheiyvanot z ricxvi: ");
+        int z = Int32.Parse(Console.ReadLine());
+        Console.WriteLine();
+        Console.Write("Migebuli shedegi= ");
+        Console.WriteLine(x + z * y);
+        Console.WriteLine();
+        Console.WriteLine();
 
-        Console.WriteLine("rogor xar");
 
-        Console.WriteLine("kargad");
+        Console.Write("saxeli: ");
+        string a = Console.ReadLine();
+        Console.Write("gvari: ");
+        string b = Console.ReadLine();
+        Console.Write("mamis saxeli: ");
+        string c = Console.ReadLine();
+        Console.Write("dabadebis tarigi: ");
+        int d = Int32.Parse(Console.ReadLine());
+
+
+        string vertical = "|";
+        string horizontal = "___";
+        Console.WriteLine();
+        Console.WriteLine(" " + " " + " " + vertical + " " + " " + " " + vertical);
+        Console.WriteLine(" " + "1" + " " + vertical + " " + "2" + " " + vertical + "3");
+        Console.WriteLine(horizontal + vertical + horizontal + vertical + horizontal);
+        Console.WriteLine(" " + " " + " " + vertical + " " + " " + " " + vertical);
+        Console.WriteLine(" " + "4" + " " + vertical + " " + "5" + " " + vertical + "6");
+        Console.WriteLine(horizontal + vertical + horizontal + vertical + horizontal);
+        Console.WriteLine(" " + " " + " " + vertical + " " + " " + " " + vertical);
+        Console.WriteLine(" " + "7" + " " + vertical + " " + "8" + " " + vertical + "9");
+        Console.WriteLine(horizontal + vertical + horizontal + vertical + horizontal);
+        Console.WriteLine(" " + " " + " " + vertical + " " + " " + " " + vertical);
 
     }
-
-    /// <summary>
-    /// komentaris adgilis
-    /// </summary>
-
+    
 }
